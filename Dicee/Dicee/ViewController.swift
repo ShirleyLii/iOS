68ec17f3f3d9e5c3cb455db07b871d6d23a9d1fc
ShirleyLii <liyingfe@msu.edu>
2019-01-28 00:00:03 -0500

    sgas

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by SHIRLEY on 1/27/19.
+//  Copyright © 2019 Shirley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
