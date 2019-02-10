e6637cec754a6a6568195dd855ca0840acf8b698
ShirleyLii <liyingfe@msu.edu>
2019-02-09 23:52:02 -0500

    get familar with swift

---
+++ variable.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var myAge : Int = 24
+
+myAge = 33
+
+let myName : String = "Shirley"
+
+let myLastName : String = "Li"
+
+let myAgeInTenYears = myAge + 10
+
+let myFullName = "\(myName) \(myLastName)"
+
+let booleans : Bool = true
+
+let floatNumer : Float = 3.3
+
+let double : Double = 2.2347876546987654
+
+
+
+
